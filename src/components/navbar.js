553dad1c8eec571591510b8b90055cd3b486c458
Dev.David <97886296+kun-lex@users.noreply.github.com>
2023-09-20 10:20:33 +0100

    update components

---
+++ src/components/navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Navbar() {
+  return (
+    <nav className='flex  justify-center items-center gap-[86px] text-white ' >
+        <h1>LOGO</h1>
+        <div>
+            <input />
+        </div>
+        <div>
+            <h4>Categories</h4>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
